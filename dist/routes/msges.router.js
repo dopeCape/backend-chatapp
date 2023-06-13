66361604b78f3b399f6cc13184004d4274f1fa4b
tejes <tmank14319@gmail.com>
2023-06-13 18:40:29 +0530

    just work dude

--- dist/routes/msges.router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.msgRouter = void 0;
-const express_1 = require("express");
-// import { handleGetAllMsg } from "../controllers/msg.controller";
-//
-let msgRouter = (0, express_1.Router)();
-exports.msgRouter = msgRouter;
--- dist/routes/workspace.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.workSpaceRouter = void 0;
-const express_1 = require("express");
-const workspace_controller_1 = require("../controllers/workspace.controller");
-const auth_middleware_1 = require("../middleware/auth.middleware");
-let workSpaceRouter = (0, express_1.Router)();
-exports.workSpaceRouter = workSpaceRouter;
-workSpaceRouter.post("/create", auth_middleware_1.verifyUser, workspace_controller_1.handleCreateWorkSpace);
