b11f5082d9da52ef2c96910933a562a44e60ff65
tejes <tmank14319@gmail.com>
2023-06-28 00:07:39 +0530

    {its just a test commit}

---
+++ push.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+mkdir api
+touch api/index.ts
+echo "import {app} from \"../src\";  export {app} " > api/index.ts
+git add .
+echo "enter the commit msg"
+read commit_msg
+git commit -m "{$commit_msg}"
+git push mym
+rm -r api
